00dfe41751252aabf8af500b62bee03224f33d3a
tpemeja <tanguy.pemeja@gmail.com>
2024-03-17 00:42:10 +0000

    Give data directly

--- app/tests/unit/test_device_api.py
+++ app/tests/unit/test_device_api.py
@@ -1,10 +1,30 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-from app.models.device import Device, DeviceDeletionResponse
+from app.models.device import DeviceDeletionResponse
 
 from app.main import app
 
-DEVICE_DATA = Device.model_config["json_schema_extra"]["example"]
+DEVICE_DATA = {
+    "device_uuid": "DEVX000001",
+    "localisation":
+        {
+            "latitude": 35.6582,
+            "longitude": 139.8752
+        },
+    "deployment_date": "2024-03-14",
+    "owner": "owner@example.com"
+}
+
+UPDATED_DEVICE_DATA = {
+    "device_uuid": "DEVX000001",
+    "localisation":
+        {
+            "latitude": 35.6582,
+            "longitude": 139.8752
+        },
+    "deployment_date": "2024-03-14",
+    "owner": "updated_owner@example.com"
+}
 
 
 def test_create_device():
@@ -27,17 +47,11 @@ def test_read_device():
 
 def test_update_device():
     # Test updating an existing device
-    updated_device_data = {
-        "device_uuid": "DEVX000001",
-        "localisation": {"latitude": 35.6582, "longitude": 139.8752},
-        "deployment_date": "2024-03-14",
-        "owner": "updated_owner@example.com"
-    }
     with TestClient(app) as client:
-        response = client.put("/devices/", json=updated_device_data)
+        response = client.put("/devices/", json=UPDATED_DEVICE_DATA)
 
     assert response.status_code == status.HTTP_200_OK  # Check status code
-    assert response.json() == updated_device_data  # Check response data
+    assert response.json() == UPDATED_DEVICE_DATA  # Check response data
 
 
 def test_delete_device():
